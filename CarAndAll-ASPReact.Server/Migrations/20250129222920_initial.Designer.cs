c1a4ff876c2a3a20f992e5ae8ba76942a6b3a152
ali <alihalisdemir@gmail.com>
2025-01-29 23:37:15 +0100

    Database reset voor assessment

--- CarAndAll-ASPReact.Server/Migrations/20250129222920_initial.Designer.cs
+++ CarAndAll-ASPReact.Server/Migrations/20250129222920_initial.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarAndAll_ASPReact.Server.Migrations
 {
     [DbContext(typeof(CarAndAllDbContext))]
-    [Migration("20250122155207_initial")]
+    [Migration("20250129222920_initial")]
     partial class initial
     {
         /// <inheritdoc />
